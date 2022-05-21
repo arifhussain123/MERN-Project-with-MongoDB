1e25c410bcbe8e5fed4eb663b9d3ccd13f861818
Arif Hussain <arifsiksavi@yahoo.com>
2022-05-22 02:13:12 +0500

    about complete

--- client/src/asst/index.js
+++ client/src/asst/index.js
@@ -1,8 +1,12 @@
 import desk from "./Image/333423.png";
+import saddiqa from ".//Image/saddiqa.png";
+// import kaneez from "./image/kaneez.png";
 // import ddddd from "./icons/333423.png";
 
 export const Image = {
   desk,
+  saddiqa,
+  // kaneez
 };
 
 export const Icons = {
--- client/src/components/About.js
+++ client/src/components/About.js
@@ -8,21 +8,23 @@ const About = () => {
         <from method="">
           <div className="row">
             <div className="col-md-4">
-              <img
-                src={Image.desk}
-                alt="Mypic"
-                className="profile-pic"
-                style={{ with: "100px", height: "100px" }}
-              />
+              <div className="profile-img">
+                <img
+                  src={Image.saddiqa}
+                  alt="Mypic"
+                  className="profile-pic"
+                  style={{ with: "100px", height: "100px" }}
+                />
+              </div>
             </div>
             <div className="col-md-6">
               <div className="profile-head">
                 <h4>My Poject</h4>
                 <h6>Web Profile </h6>
-                <p className="proile-rating mt-3 mb-3">
-                  Rating : <span>8/10</span>
+                <p className="profile-rating mt-3 mb-5">
+                  RANKINGS : <span>8/10</span>
                 </p>
-                <ul className="nav nav-tabs" id="myTab" role="tablist">
+                <ul className="nav nav-tabs" role="tablist">
                   <li className="nav-item">
                     <a
                       className="nav-link active"
@@ -30,24 +32,19 @@ const About = () => {
                       data-toggle="tab"
                       href="#home"
                       role="tab"
-                      aria-controls="home"
-                      aria-selected="true"
                     >
                       About
                     </a>
                   </li>
                   <li className="nav-item">
-                    
                     <a
-                      className="nav-link active "
+                      className="nav-link active"
                       id="profile-tab"
                       data-toggle="tab"
                       href="#profile"
                       role="tab"
-                      aria-controls="profile"
-                      aria-selected="false"
                     >
-                      Timeline
+                      Profile
                     </a>
                   </li>
                 </ul>
@@ -66,25 +63,94 @@ const About = () => {
             <div className="col-md-4">
               <div className="profile-work">
                 <h2>WORK LINK</h2>
-                <a href="">Website Link</a>
+                <a href="#f">Website Link</a>
                 <br />
-                <a href="">Bootsnipp Profile</a>
+                <a href="#f">Bootsnipp Profile</a>
                 <br />
-                <a href="">Bootply Profile</a>
+                <a href="#f">Bootply Profile</a>
                 {/* <p>SKILLS</p> */}
-                <a href="">Web Designer</a>
+                <a href="#f">Web Designer</a>
                 <br />
-                <a href="">Web Developer</a>
+                <a href="#f">Web Developer</a>
                 <br />
-                <a href="">WordPress</a>
+                <a href="#f">WordPress</a>
                 <br />
-                <a href="">WooCommerce</a>
+                <a href="#f">WooCommerce</a>
                 <br />
-                <a href="">PHP, .Net</a>
+                <a href="#f">PHP, .Net</a>
                 <br />
               </div>
             </div>
-            {/* <div className="col-md-8">
+
+            <div className="col-md-8 pl-5 about-info">
+              <div className="tab-content profile-tab" id="myTabContent ">
+                <div
+                  className="tab-pane fade show active"
+                  id="home"
+                  role="tabpanel"
+                  area-labelledby="home-tab"
+                >
+                  <div className="row mt-5">
+                    <div className="col-md-6">
+                      <label>User ID</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>1252525252</p>
+                    </div>
+                  </div>
+                  <div className="row ">
+                    <div className="col-md-6">
+                      <label>Name</label>
+                    </div>
+                    <div className="col-md-6">
+                      <label>Arif Murtaza</label>
+                    </div>
+                  </div>
+                  <div className="row ">
+                    <div className="col-md-6">
+                      <label>Name</label>
+                    </div>
+                    <div className="col-md-6">
+                      <label>Arif Murtaza</label>
+                    </div>
+                  </div>{" "}
+                  <div className="row ">
+                    <div className="col-md-6">
+                      <label>Name</label>
+                    </div>
+                    <div className="col-md-6">
+                      <label>Arif Murtaza</label>
+                    </div>
+                  </div>{" "}
+                  <div className="row ">
+                    <div className="col-md-6">
+                      <label>Name</label>
+                    </div>
+                    <div className="col-md-6">
+                      <label>Arif Murtaza</label>
+                    </div>
+                  </div>{" "}
+                  <div className="row ">
+                    <div className="col-md-6">
+                      <label>Name</label>
+                    </div>
+                    <div className="col-md-6">
+                      <label>Arif Murtaza</label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </from>
+      </div>
+    </>
+  );
+};
+
+export default About;
+
+/* <div className="col-md-8">
               <div className="tab-content profile-tab" id="myTabContent">
                 <div
                   className="tab-pane fade show active"
@@ -115,12 +181,4 @@ const About = () => {
                   </div>
                 </div>
               </div>
-            </div> */}
-          </div>
-        </from>
-      </div>
-    </>
-  );
-};
-
-export default About;
+            </div> */
